3102c9a73f2a7c2f4b11bcb75c1ef7963db527ee
Sergio Berdeja <sergioberdeja@gmail.com>
2022-12-14 18:05:22 -0700

    create first class

---
+++ Rain.java
@@ -0,0 +1,12 @@
+package com.it.berdeja.simplePhysics;
+
+import java.util.Arrays;
+
+public class Rain{
+	public static void main(String[] args) {
+		int[][] emptySky = new int[10][10];
+		for (int i = 0; i < emptySky.length; i++) {
+			System.out.println(Arrays.toString(emptySky[i]));			
+		}
+	}
+}
