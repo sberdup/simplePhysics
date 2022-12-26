79d20a98dcb27f6807f8acb048bcaae623da60dd
Sergio Berdeja <sergioberdeja@gmail.com>
2022-12-25 17:38:52 -0700

    add string examples

---
+++ SplitWords.java
@@ -0,0 +1,13 @@
+package com.it.berdeja.simplePhysics;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SplitWords {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please, enter any text: ");
+		String userInput = sc.nextLine();
+		System.out.print("You entered these words: ");
+		System.out.println(Arrays.toString(userInput.split("[\\.\\?\\!\\s]+")));
+	}
+}
