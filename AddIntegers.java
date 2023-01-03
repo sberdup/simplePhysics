ab5092f007d854fa6c1c858881c0a46655b439fa
Sergio Berdeja <sergioberdeja@gmail.com>
2023-01-02 17:14:06 -0700

    remove classes

--- AddIntegers.java
+++
@@ -1,9 +0,0 @@
-package com.it.berdeja.simplePhysics;
-
-public class AddIntegers {
-	public static void main(String[] args) {
-		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);
-		System.out.println(a + b);
-	}
-}
